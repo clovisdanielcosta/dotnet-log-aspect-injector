740730930c24aac65ae398d611b42ad17e6efdba
Clovis Daniel Costa <clovisdanielcosta@gmail.com>
2024-09-30 11:58:51 -0300

    Add project files.

---
+++ LogCall.cs
@@ -0,0 +1,38 @@
+﻿using AspectInjector.Broker;
+using System.Diagnostics;
+using System.Reflection;
+using Scope = AspectInjector.Broker.Scope;
+
+namespace LogAspectInjector;
+
+[Aspect(Scope.Global)]
+[Injection(typeof(LogCall))]
+public class LogCall : Attribute
+{
+    private static Stopwatch _watch = new ();
+
+    [Advice(Kind.Before)]
+    public void LogBefore([Argument(Source.Name)] string name, [Argument(Source.Metadata)] MethodBase method)
+    {
+        if (name.Equals(".ctor")) return;
+
+        var className = method.DeclaringType.Name;
+
+        if (method.IsPublic) _watch.Start();
+    }
+
+    [Advice(Kind.After)]
+    public void LogAfter([Argument(Source.Name)] string name, [Argument(Source.Metadata)] MethodBase method) 
+    {
+        if (name.Equals(".ctor")) return;
+
+        var className = method.DeclaringType.Name;
+
+        if (method.IsPublic)
+        {
+            _watch.Stop();
+            Console.WriteLine($"Classe: {className, -20} | Método: {name, -20} | ElapsedTime: {_watch.Elapsed}");
+            _watch.Reset();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LogAspectInjector;
+
+TesteLogWithAspectInjector.Test1();
+TesteLogWithAspectInjector.Test2();
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+[LogCall]
+public static class TesteLogWithAspectInjector
+{
+    public static void Test1()
+    {
+        
+    }
+
+    public static void Test2()
+    {
+        
+    }
+}
